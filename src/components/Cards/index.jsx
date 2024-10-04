fd2718462e266a97036655e201e955bb1bc59d37
William Dias <smdwilliam89@gmail.com>
2024-10-04 10:45:25 -0300

    feat(Cards): Adicionando o componente Cards e o Fetch para consumir as informações do arquivo JSON.

---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,32 @@
+import './styles.css';
+
+import React, { useEffect, useState } from 'react';
+
+const Cards = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('/public/data.json')
+            .then((response) => response.json())
+            .then((data) => setProducts(data))
+            .catch((error) => console.error('Erro ao carregar dados', error));
+    }, []);
+
+    return (
+        <>
+            <h1>Desserts</h1>
+            <div className="product-list">
+                {products.map((product, index) => (
+                    <div key={index} className="product">
+                        <img src={product.image.thumbnail} alt={product.name} />
+                        <p>{product.category}</p>
+                        <h2>{product.name}</h2>
+                        <p>${product.price.toFixed(2)}</p>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default Cards;

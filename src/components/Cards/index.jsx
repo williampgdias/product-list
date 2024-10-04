b8ddfd1165d9e40f9462e15c031ea1e379d9ca7e
William Dias <smdwilliam89@gmail.com>
2024-10-04 13:59:23 -0300

    style(Card): Adicionando estilo nos Cards.

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -2,6 +2,10 @@ import './styles.css';
 
 import React, { useEffect, useState } from 'react';
 
+// import images
+import emptyCart from '../../../public/assets/images/illustration-empty-cart.svg';
+import addToCart from '../../../public/assets/images/icon-add-to-cart.svg';
+
 const Cards = () => {
     const [products, setProducts] = useState([]);
 
@@ -14,17 +18,63 @@ const Cards = () => {
 
     return (
         <>
-            <h1>Desserts</h1>
-            <div className="product-list">
-                {products.map((product, index) => (
-                    <div key={index} className="product">
-                        <img src={product.image.thumbnail} alt={product.name} />
-                        <p>{product.category}</p>
-                        <h2 className="">{product.name}</h2>
-                        <p>${product.price.toFixed(2)}</p>
+            <main className="productList">
+                <h1 className="title">Desserts</h1>
+                <div className="productsContainer">
+                    {/* Seção de Produtos */}
+                    {/* Products Section */}
+                    <div className="cardsContainer">
+                        {products.map((product, index) => (
+                            <div key={index} className="card">
+                                <img
+                                    className="dessertsImage"
+                                    src={product.image.desktop}
+                                    alt={product.name}
+                                />
+                                <div className="cartButtonContainer">
+                                    <button className="cartButton">
+                                        <img
+                                            className="mr-2"
+                                            src={addToCart}
+                                            alt="Add to cart"
+                                        />
+                                        <p className="lg:text-sm xl:text-xs 2xl:text-base">
+                                            Add to Cart
+                                        </p>
+                                    </button>
+                                </div>
+                                <div className="dessertInfo">
+                                    <p className="productCategory">
+                                        {product.category}
+                                    </p>
+                                    <h2 className="productName">
+                                        {product.name}
+                                    </h2>
+                                    <p className="productPrice">
+                                        ${product.price.toFixed(2)}
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
+
+                    {/* Seção do Carrinho */}
+                    {/* Cart Section */}
+                    <div className="h-fit border rounded-lg p-4 shadow-lg">
+                        <h2 className="text-2xl text-[#B23515] font-semibold mb-4">
+                            Your Cart (0)
+                        </h2>
+                        <img
+                            className="mx-auto mb-4"
+                            src={emptyCart}
+                            alt="Empty cart"
+                        />
+                        <p className="text-center text-sm text-[#B18F8B]">
+                            Your added items will appear here
+                        </p>
+                    </div>
+                </div>
+            </main>
         </>
     );
 };
